9d72218104dfad0ab8be6a34bcddac3eff7ee26e
fabroche <kraxusmmo@gmail.com>
2025-09-02 20:47:53 +0100

    feat: RootLayout setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,17 @@ import {montserrat} from "@/app/ui/fonts";
 import "./ui/global.css";
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body
+            className={`${montserrat.className} antialiased`}
+        >
+        {children}
+        </body>
+        </html>
+    );
 }
